9896f6c4eed07a17f1156747c0afb5f93222ae61
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2023-01-09 09:51:29 +0100

    new front page

--- scroll.js
+++ scroll.js
@@ -8,3 +8,22 @@ function myFunction() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.height = scrolled + "%";
 }
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.centered');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.centered .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 500,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.centered',
+    opacity: 0,
+    duration: 500,
+    easing: "easeOutExpo",
+    delay: 100000
+  });
